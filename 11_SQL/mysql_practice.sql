b51275ea0fadef70436d15624580023441f47ce8
고 세진 <ko880428@gmail.com>
2022-02-21 22:42:05 +0900

    mysql 4,5,6 study

--- 11_SQL/mysql_practice.sql
+++ 11_SQL/mysql_practice.sql
@@ -49,4 +49,104 @@ and purchased_at < '2020-08-01 00:00:00';
 
 select round((11174/16414)*100,2);
 
--- 
+-- 월별 구매율 보기
+
+select * from fast.visit;
+select count(customer_id) from fast.visit;
+
+select 
+	v2.visited_month,
+	count(v.customer_id)
+from 
+	fast.visit v 
+	inner join (select
+					customer_id,
+					date_format(visited_at, '%Y-%m') as visited_month
+				from 
+					fast.visit) v2
+	on v.customer_id = v2.customer_id
+group by
+	1;
+
+select 
+	*
+from 
+	fast.visit v 
+	left join (select
+					customer_id,
+					date_format(visited_at, '%m') as visited_month
+				from 
+					fast.visit) v2
+	on v.customer_id = v2.customer_id;
+
+
+-- 4. 7월에 구매 유저의 월 평균 구매금액은 어떻게 되나요?
+
+select * from fast.purchase p ;
+
+select
+	avg(db.revenue)
+from
+	(select
+		customer_id,
+		sum(price) as revenue
+	from
+		fast.purchase p 
+	where 
+		purchased_at >= '2020-07-01 00:00:00'
+	and purchased_at < '2020-08-01 00:00:00'
+	group by 
+		1) as db
+
+
+-- 5. 7월에 가장 많이 구매한 고객 Top3와 Top10~15 고객을 뽑아주세요
+		
+select * from fast.purchase;
+
+select 
+	customer_id,
+	count(id)
+from 
+	fast.purchase p 
+where 
+	purchased_at >= '2020-07-01 00:00:00'
+and purchased_at < '2020-08-01 00:00:00'
+group by
+	1
+order by
+	2 desc
+limit 3;
+	
+select 
+	customer_id,
+	count(id)
+from 
+	fast.purchase p 
+where 
+	purchased_at >= '2020-07-01 00:00:00'
+and purchased_at < '2020-08-01 00:00:00'
+group by
+	1
+order by
+	2 desc
+limit
+	5 offset 10;
+	
+
+-- 6. 2020년 7월의 평균 DAU를 구해주세요, Activity User 수가 추세 증가하는 추세인가요?
+select * from fast.visit v ;
+
+select
+ 	date_format(visited_at, '%Y-%m-%d') as date_at,
+ 	count(customer_id)
+from 
+	fast.visit v
+where 
+	visited_at >= '2020-07-01 00:00:00'
+and visited_at < '2020-08-01 00:00:00'
+group by 
+	1
+;
+
+	
+ 
